97f29444a3b05d871e2488ca9f68c922f5b33858
ねむー <nemurin@users.noreply.github.com>
2018-11-20 02:35:41 +0900

    Solve AtCoder Beginner Contest 113 A question

---
+++ ABC113-A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ABC113-A
+            string[] temp = Console.ReadLine().Split(' ');
+            int AtoB = int.Parse(temp[0]);
+            int BtoC = int.Parse(temp[1]) / 2;
+            Console.WriteLine(AtoB+BtoC);
+        }
+    }
+}
